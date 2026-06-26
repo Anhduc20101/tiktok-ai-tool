ab8be7e1f7dd8547a3008ec7c9f3991a57c409ed
Anhduc20101 <phamduc90h3@gmail.com>
2026-06-27 03:33:44 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ app.post("/generate", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("🚀 FREE AI Server chạy tại http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Server running on port " + PORT);
 });
